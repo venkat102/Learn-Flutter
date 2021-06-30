26f65bf162a66e600eec96160dbb81ce29941471
venkat102 <venkatesharunachalam659@gmail.com>
2021-06-30 21:06:55 +0530

    feat: add card to the page

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,32 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Card'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[_getCard()],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container _getCard() {
     return Container(
-      child: null,
+      width: 350,
+      height: 200,
+      decoration: BoxDecoration(
+        color: Colors.pinkAccent,
+        borderRadius: BorderRadius.circular(3.5)
+      ),
     );
   }
 }
